98d24c665ce135b2c35748a78c59f3217bd4c0b7
arborelia <gh@arborelia.net>
2020-12-01 00:40:00 -0500

    Advent of code day 1

---
+++ advent1/src/main.rs
@@ -0,0 +1,39 @@
+mod helpers;
+use helpers::*;
+use std::io::{Result};
+
+fn find_pair_product(numbers: &Vec<i64>) -> Option<i64> {
+    for pos1 in 0..(numbers.len() - 1) {
+        for pos2 in (pos1 + 1)..(numbers.len()) {
+            if numbers[pos1] + numbers[pos2] == 2020 {
+                return Some(numbers[pos1] * numbers[pos2]);
+            }
+        }
+    }
+    None
+}
+
+fn find_triple_product(numbers: &Vec<i64>) -> Option<i64> {
+    let n = numbers.len();
+    for pos1 in 0..n {
+        for pos2 in (pos1 + 1)..n {
+            for pos3 in (pos2 + 1)..n {
+                if numbers[pos1] + numbers[pos2] + numbers[pos3] == 2020 {
+                    return Some(numbers[pos1] * numbers[pos2] * numbers[pos3]);
+                }
+            }
+        }
+    }
+    None
+}
+
+fn main() -> Result<()> {
+    let mut numbers: Vec<i64> = Vec::new();
+    for line in read_lines("/home/elia/win/Downloads/input1.txt") {
+        let num: i64 = line?.parse().unwrap();
+        numbers.push(num);
+    }
+    let product = find_triple_product(&numbers).expect("No triple adds to 2020");
+    println!("{}", product);
+    Ok(())
+}
