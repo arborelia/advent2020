2b540e9c3e97a74230e5f8dad45d9229e5898dd8
arborelia <gh@arborelia.net>
2020-12-31 02:42:15 -0500

    apparently-working data structure for day 23

--- advent23/src/main.rs
+++ advent23/src/main.rs
@@ -1,5 +1,4 @@
 use std::cell::RefCell;
-use std::collections::VecDeque;
 use std::rc::Rc;
 
 /// A circular linked list of consecutive integers, that remembers where each of the
@@ -14,23 +13,11 @@ impl SpinnyListCell {
         self.val
     }
 
-    fn rest(&self) -> Option<Rc<Self>> {
+    fn next(&self) -> Rc<Self> {
         match *self.tail.borrow() {
-            Some(ref tail) => Some(tail.clone()),
-            None => None,
+            Some(ref tail) => tail.clone(),
+            None => panic!("reached the end of the SpinnyList somehow"),
         }
-    }
-
-    fn insert_slice(&mut self, slice: &[u32]) {
-        let mut current: Option<Rc<SpinnyListCell>> = self.tail.borrow().clone();
-        for &val in slice.iter().rev() {
-            let cell = SpinnyListCell {
-                val,
-                tail: RefCell::new(current),
-            };
-            current = Some(Rc::new(cell));
-        }
-        *self.tail.borrow_mut() = current;
     }
 }
 
@@ -44,7 +31,6 @@ impl SpinnyList {
         let size = slice.len();
         let mut index: Vec<Option<Rc<SpinnyListCell>>> = vec![None; size];
 
-        // let mut end_of_list: Option<Rc<RefCell<Option<SpinnyListCell>>>> = None;
         let mut current: Option<Rc<SpinnyListCell>> = None;
         let mut end_of_list: Option<Rc<SpinnyListCell>> = None;
         for &val in slice.iter().rev() {
@@ -66,6 +52,34 @@ impl SpinnyList {
         SpinnyList { head, index }
     }
 
+    fn move_next_chunk(&mut self, chunk_size: usize, dest_val: u32) {
+        let head_cell = self.head.clone().unwrap();
+        let startpoint = head_cell.next();
+        let mut penultimate = startpoint.clone();
+        let mut values_being_moved: Vec<u32> = vec![startpoint.val];
+
+        for _i in 0..(chunk_size - 1) {
+            penultimate = penultimate.next();
+            values_being_moved.push(penultimate.val);
+        }
+
+        let mut dest_val = dest_val;
+        while values_being_moved.contains(&dest_val) {
+            dest_val -= 1;
+            if dest_val == 0 {
+                dest_val = self.len() as u32;
+            }
+        }
+        let destination = self.find_cell(dest_val).unwrap();
+        let destination_next = destination.next();
+
+        let endpoint = penultimate.next();
+
+        *head_cell.tail.borrow_mut() = Some(endpoint.clone());
+        *destination.tail.borrow_mut() = Some(startpoint.clone());
+        *penultimate.tail.borrow_mut() = Some(destination_next.clone());
+    }
+
     pub fn find_cell(&self, val: u32) -> Option<Rc<SpinnyListCell>> {
         self.index[(val - 1) as usize].clone()
     }
@@ -74,19 +88,12 @@ impl SpinnyList {
         self.head.as_ref().expect("list was empty").value()
     }
 
-    pub fn next(&mut self) -> u32 {
-        let val = self.head.as_ref().unwrap().val;
-        let nextref = self
-            .head
-            .as_ref()
-            .expect("why is the SpinnyList empty")
-            .rest();
-        self.head = Some(nextref.expect("reached the end of the SpinnyList somehow"));
-        val
-    }
-
     pub fn jump_to(&mut self, val: u32) {
         self.head = self.find_cell(val);
+    }
+
+    pub fn step_to_next(&mut self) {
+        self.head = Some(self.head.as_ref().unwrap().next())
     }
 
     pub fn len(&self) -> usize {
@@ -96,91 +103,51 @@ impl SpinnyList {
     /// "Unwind" the SpinnyList into a vector.
     pub fn to_vec(&mut self) -> Vec<u32> {
         let mut vals: Vec<u32> = Vec::new();
+        let mut cell = self.head.clone().expect("Empty lists aren't supported");
         while vals.len() < self.len() {
-            let next_val = self.next();
-            vals.push(next_val);
+            vals.push(cell.value());
+            cell = cell.next();
         }
         vals
     }
 }
 
-fn perform_cup_move_new(cups: &mut SpinnyList) {
-    let highest_cup = cups.len() as u32;
-    let current_cup = cups.first();
-
-    // cups.
+fn perform_cup_move(cups: &mut SpinnyList) {
+    let first_cup: u32 = cups.first();
+    let destination: u32 = if first_cup == 1 {
+        cups.len() as u32
+    } else {
+        first_cup - 1
+    };
+    cups.move_next_chunk(3, destination);
+    cups.step_to_next();
 }
 
-fn perform_cup_move(cups: &mut VecDeque<u32>) {
-    let highest_cup = cups.len() as u32;
-    let current_cup = cups.pop_front().unwrap();
-    let mut picked_up: Vec<u32> = Vec::new();
-    for _i in 0..3 {
-        picked_up.push(cups.pop_front().unwrap());
-    }
-    let mut target_cup = current_cup - 1;
-    if target_cup == 0 {
-        target_cup = highest_cup;
-    }
-    while picked_up.contains(&target_cup) {
-        target_cup -= 1;
-        if target_cup == 0 {
-            target_cup = highest_cup;
-        }
-    }
-
-    let target_index = cups.iter().position(|&x| x == target_cup).unwrap();
-    for _i in 0..3 {
-        cups.insert(target_index + 1, picked_up.pop().unwrap());
-    }
-
-    cups.push_back(current_cup);
-}
-
-fn order_after_n_moves(mut cups: VecDeque<u32>, count: usize) -> Vec<u32> {
+fn order_after_n_moves(cups: &mut SpinnyList, count: u32) -> Vec<u32> {
     for iter in 0..count {
         if iter % 1000 == 0 {
             dbg!(iter);
         }
-        perform_cup_move(&mut cups);
+        perform_cup_move(cups);
     }
-    loop {
-        let first_cup = cups.pop_front().unwrap();
-        if first_cup == 1 {
-            break;
-        }
-        cups.push_back(first_cup);
-    }
-    cups.iter().cloned().collect()
+    cups.jump_to(1);
+    let mut order = cups.to_vec();
+    order.remove(0);
+    order
 }
 
 fn main() {
-    let cups: VecDeque<u32> = VecDeque::from(vec![5, 8, 9, 1, 7, 4, 2, 6, 3]);
-    let order = order_after_n_moves(cups, 100);
+    let mut cups: SpinnyList = SpinnyList::from_slice(&[5, 8, 9, 1, 7, 4, 2, 6, 3]);
+    let order = order_after_n_moves(&mut cups, 100);
     println!("Cup order for part 1: {:?}", order);
 
-    let mut cups: VecDeque<u32> = VecDeque::from(vec![5, 8, 9, 1, 7, 4, 2, 6, 3]);
+    let mut cup_vec: Vec<u32> = vec![5, 8, 9, 1, 7, 4, 2, 6, 3];
     for cup in 10..=1_000_000 {
-        cups.push_back(cup);
+        cup_vec.push(cup);
     }
-    let order = order_after_n_moves(cups, 10_000_000);
+    let mut cups: SpinnyList = SpinnyList::from_slice(&cup_vec);
+    let order = order_after_n_moves(&mut cups, 10_000_000);
     println!("Product for part 2: {}", order[0] * order[1]);
-}
-
-#[test]
-fn test_individual_moves() {
-    let mut cups: VecDeque<u32> = VecDeque::from(vec![3, 8, 9, 1, 2, 5, 4, 6, 7]);
-    perform_cup_move(&mut cups);
-    assert_eq!(Vec::from(cups.clone()), vec![2, 8, 9, 1, 5, 4, 6, 7, 3]);
-    perform_cup_move(&mut cups);
-    assert_eq!(Vec::from(cups), vec![5, 4, 6, 7, 8, 9, 1, 3, 2]);
-}
-
-#[test]
-fn test_100_moves() {
-    let cups: VecDeque<u32> = VecDeque::from(vec![3, 8, 9, 1, 2, 5, 4, 6, 7]);
-    let answer = order_after_n_moves(cups, 100);
-    assert_eq!(answer, vec![6, 7, 3, 8, 4, 5, 2, 9]);
 }
 
 #[test]
@@ -201,4 +168,27 @@ fn test_find_values() {
     spinny.jump_to(3);
     assert_eq!(spinny.to_vec(), vec![3, 2, 4, 5, 1]);
     assert_eq!(spinny.find_cell(4).unwrap().val, 4);
+}
+
+#[test]
+fn test_move_chunk() {
+    let mut spinny = SpinnyList::from_slice(&[5, 1, 3, 2, 4, 7, 6]);
+    spinny.move_next_chunk(3, 7);
+    assert_eq!(spinny.to_vec(), vec![5, 4, 7, 1, 3, 2, 6]);
+}
+
+#[test]
+fn test_individual_moves() {
+    let mut cups: SpinnyList = SpinnyList::from_slice(&[3, 8, 9, 1, 2, 5, 4, 6, 7]);
+    perform_cup_move(&mut cups);
+    assert_eq!(cups.to_vec(), vec![2, 8, 9, 1, 5, 4, 6, 7, 3]);
+    perform_cup_move(&mut cups);
+    assert_eq!(cups.to_vec(), vec![5, 4, 6, 7, 8, 9, 1, 3, 2]);
+}
+
+#[test]
+fn test_100_moves() {
+    let mut cups: SpinnyList = SpinnyList::from_slice(&[3, 8, 9, 1, 2, 5, 4, 6, 7]);
+    let answer = order_after_n_moves(&mut cups, 100);
+    assert_eq!(answer, vec![6, 7, 3, 8, 4, 5, 2, 9]);
 }
